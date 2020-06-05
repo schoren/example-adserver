1ca146b87cc0ff6db7bac0fe09778bdd55444e67
Sebastian Choren <sebastian.choren@gmail.com>
2020-06-05 18:59:56 -0300

    Implement main to tie all toghether

--- ads/main.go
+++ ads/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/schoren/example-adserver/ads/internal/commands"
+	"github.com/schoren/example-adserver/ads/internal/handlers"
+	"github.com/schoren/example-adserver/ads/internal/platform/mysql"
+)
+
 func main() {
 
+	dbDSN := os.Getenv("DB_DSN")
+	if dbDSN == "" {
+		panic(fmt.Errorf("DB_DSN not provided"))
+	}
+
+	srvAddr := os.Getenv("SRV_ADDR")
+	if srvAddr == "" {
+		panic(fmt.Errorf("SRV_ADDR not provided"))
+	}
+
+	db, err := sql.Open("mysql", dbDSN)
+	if err != nil {
+		panic(fmt.Errorf("cannot connect to mysql database: %w", err))
+	}
+	defer db.Close()
+
+	router := mux.NewRouter()
+
+	adsRepository := mysql.NewAdsRepository(db)
+
+	handlers.CreateCommand = &commands.Create{
+		Persister: adsRepository,
+	}
+
+	handlers.UpdateCommand = &commands.Update{
+		Persister: adsRepository,
+	}
+
+	handlers.ConfigureRouter(router.PathPrefix("/ads").Subrouter())
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    srvAddr,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 2 * time.Second,
+		ReadTimeout:  2 * time.Second,
+	}
+
+	log.Printf("Starting server on %s", srvAddr)
+	log.Fatal(srv.ListenAndServe())
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/schoren/example-adserver
 go 1.14
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.7.4
 	github.com/stretchr/testify v1.6.1
 )
