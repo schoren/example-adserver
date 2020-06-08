97c9e3f0295f9847f7f44ae9a50353ee65ca1ca4
Sebastian Choren <sebastian.choren@gmail.com>
2020-06-08 14:43:57 -0300

    Cleanup ads main

--- ads/main.go
+++ ads/main.go
@@ -5,80 +5,102 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/Shopify/sarama"
+	"github.com/caarlos0/env/v6"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/schoren/example-adserver/ads/internal/commands"
 	"github.com/schoren/example-adserver/ads/internal/handlers"
 	"github.com/schoren/example-adserver/ads/internal/platform/kafka"
 	"github.com/schoren/example-adserver/ads/internal/platform/mysql"
+	"gopkg.in/validator.v2"
 )
 
+type config struct {
+	AdserverBaseURL       string   `env:"ADSERVER_BASE_URL" validate:"nonzero"`
+	DBDSN                 string   `env:"DB_DSN" validate:"nonzero"`
+	SrvAddr               string   `env:"SRV_ADDR" validate:"nonzero"`
+	KafkaBootstrapServers []string `env:"KAFKA_BOOTSTRAP_SERVERS" envSeparator:"," validate:"nonzero"`
+}
+
+func readEnvConfig(cfg interface{}) {
+	if err := env.Parse(cfg); err != nil {
+		panic(fmt.Errorf("cannot parse env config: %w", err))
+	}
+
+	if errs := validator.Validate(cfg); errs != nil {
+		panic(fmt.Errorf("invalid env config: %w", errs))
+	}
+}
+
 func main() {
-
-	adserverBaseURL := os.Getenv("ADSERVER_BASE_URL")
-	if adserverBaseURL == "" {
-		panic(fmt.Errorf("ADSERVER_BASE_URL not provided"))
-	}
-
-	dbDSN := os.Getenv("DB_DSN")
-	if dbDSN == "" {
-		panic(fmt.Errorf("DB_DSN not provided"))
-	}
-
-	srvAddr := os.Getenv("SRV_ADDR")
-	if srvAddr == "" {
-		panic(fmt.Errorf("SRV_ADDR not provided"))
-	}
-
-	kafkaBootstrapServers := os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
-	if kafkaBootstrapServers == "" {
-		panic(fmt.Errorf("SRV_ADDR not provided"))
-	}
-
-	db, err := sql.Open("mysql", dbDSN)
-	if err != nil {
-		panic(fmt.Errorf("cannot connect to mysql database: %w", err))
-	}
-	defer db.Close()
-
-	config := sarama.NewConfig()
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	config.Producer.Retry.Max = 10
-	config.Producer.Return.Successes = true
-
 	// Ugly fix for docker-compose start order: just wait a few secs for kafka to be ready
 	time.Sleep(10 * time.Second)
-	log.Println("Waited enough, try to connect to", strings.Split(kafkaBootstrapServers, ","))
-	kafkaProducer, err := sarama.NewSyncProducer(strings.Split(kafkaBootstrapServers, ","), config)
-	if err != nil {
-		panic(fmt.Errorf("cannot connect to kafka bootstrap servers: %w", err))
-	}
+	log.Println("Waited enough, try to connect to", cfg.KafkaBootstrapServers)
+
+	cfg := config{}
+
+	readEnvConfig(&cfg)
+
+	db := openDBConnection(cfg)
+	defer db.Close()
+
+	kafkaProducer := connectKafkaProducer(cfg)
 	defer kafkaProducer.Close()
 
-	adsRepository := mysql.NewAdsRepository(db)
-	kafkaNotifier := kafka.NewNotifier(kafkaProducer)
+	setupHandlers(cfg, db, kafkaProducer)
+	srv := setupHTTPServer(cfg)
 
-	handlers.AdServerBaseURL = adserverBaseURL
+	log.Printf("Starting server on %s", srv.Addr)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func setupHandlers(cfg config, db *sql.DB, kafkaProducer sarama.SyncProducer) {
+	kafkaNotifier := kafka.NewNotifier(kafkaProducer)
+	adsRepository := mysql.NewAdsRepository(db)
+
+	handlers.AdServerBaseURL = cfg.AdserverBaseURL
 	handlers.CreateCommand = commands.NewCreate(adsRepository, kafkaNotifier)
 	handlers.UpdateCommand = commands.NewUpdate(adsRepository, kafkaNotifier)
 	handlers.ListActiveCommand = commands.NewListActive(adsRepository)
 
+}
+
+func setupHTTPServer(cfg config) *http.Server {
 	router := mux.NewRouter()
 	handlers.ConfigureRouter(router.PathPrefix("/ads").Subrouter())
 
 	srv := &http.Server{
 		Handler: router,
-		Addr:    srvAddr,
+		Addr:    cfg.SrvAddr,
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 2 * time.Second,
 		ReadTimeout:  2 * time.Second,
 	}
+}
 
-	log.Printf("Starting server on %s", srvAddr)
-	log.Fatal(srv.ListenAndServe())
+func openDBConnection(cfg config) *sql.DB {
+	db, err := sql.Open("mysql", cfg.DBDSN)
+	if err != nil {
+		panic(fmt.Errorf("cannot connect to mysql database: %w", err))
+	}
+
+	return db
+}
+
+func connectKafkaProducer(cfg config) sarama.SyncProducer {
+	config := sarama.NewConfig()
+
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 10
+	config.Producer.Return.Successes = true
+	kafkaProducer, err := sarama.NewSyncProducer(cfg.KafkaBootstrapServers, config)
+
+	if err != nil {
+		panic(fmt.Errorf("cannot connect to kafka bootstrap servers: %w", err))
+	}
+
+	return kafkaProducer
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.14
 
 require (
 	github.com/Shopify/sarama v1.26.4
+	github.com/caarlos0/env/v6 v6.2.2
 	github.com/go-resty/resty/v2 v2.3.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.4
 	github.com/stretchr/testify v1.6.1
 	gopkg.in/resty.v1 v1.12.0
+	gopkg.in/validator.v2 v2.0.0-20200605151824-2b28d334fa05
 )
